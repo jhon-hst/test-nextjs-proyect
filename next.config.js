88b0e5404d92186f5afec0d6d06eeb03334c693d
Jhon Sanchez <jhonhst2@gmail.com>
2022-12-13 13:25:18 -0500

    added other route

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  headers: () => [{
-    source: "/.well-known/apple-app-site-association",
-    headers: [{
-      key: "Content-Type",
-      value: "application/json"
-    }]
-  }]
+  headers: () => [
+    {
+      source: "/.well-known/apple-app-site-association",
+      headers: [{
+        key: "Content-Type",
+        value: "application/json"
+      }]
+    },
+    {
+      source: "/apple-app-site-association",
+      headers: [{
+        key: "Content-Type",
+        value: "application/json"
+      }]
+    }
+  ]
 }
 
 module.exports = nextConfig
