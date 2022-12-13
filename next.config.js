6066338a3700574dac5306c27d4ec80b8f264e05
Jhon Sanchez <jhonhst2@gmail.com>
2022-12-13 13:00:14 -0500

    Fixed issue with route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: () => [{
+    source: "/.well-known/apple-app-site-association",
+    headers: [{
+      key: "Content-Type",
+      value: "application/json"
+    }]
+  }]
 }
 
 module.exports = nextConfig
